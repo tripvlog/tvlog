a55847b772b02cec2980632dcdd0152f3886331d
user1 <user1@KH1>
2016-04-27 12:46:00 +0900

    first setting success

--- src/project/vlog/bean/MainBean.java
+++ src/project/vlog/bean/MainBean.java
@@ -1,5 +1,18 @@
 package project.vlog.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.ibatis.SqlMapClientTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainBean {
 
+	@Autowired
+	private SqlMapClientTemplate sqlMap;
+	
+	@RequestMapping("/test.trip")
+	public String test(){
+		return "/test/kh.jsp";
+	}
 }
