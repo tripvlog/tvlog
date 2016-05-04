8a4fab333d32f558b56704feb3b5130665bc2047
user1 <user1@KH1>
2016-05-04 09:23:13 +0900

    로그인5.4

---
+++ src/trip/member/LoginController.java
@@ -0,0 +1,86 @@
+package trip.member;
+
+
+
+	import java.nio.file.Path;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.orm.ibatis.SqlMapClientTemplate;
+	import org.springframework.stereotype.Controller;
+	import org.springframework.web.bind.annotation.RequestMapping;
+	import org.springframework.web.bind.annotation.RequestMethod;
+
+	@Controller
+	public class LoginController {
+		
+		@Autowired
+		SqlMapClientTemplate sqlmap;
+		
+		@RequestMapping("/loginForm.trip")
+		public String loginForm(){
+			return "/trip.member/loginForm.jsp";
+		}
+
+		@RequestMapping(value="/loginPro.trip",method=RequestMethod.POST)
+		public String Login(HttpSession session, LoginDTO dto,HttpServletRequest request){
+			
+				int count = (Integer)sqlmap.queryForObject("loginCheck",dto);
+				if(count==1){			
+				
+					session.setAttribute("memId",dto.getId());
+					dto=(LoginDTO)sqlmap.queryForObject("path", dto);
+					String imgPath = "D:\\save\\" + dto.getPath();
+					request.setAttribute("imgPath", imgPath);
+					request.setAttribute("dto",dto);
+									
+				}				
+				return "/trip.member/loginPro.jsp";
+					
+		}
+		
+		@RequestMapping("/logout.trip")
+		public String logout(){
+			return "/trip.member/logout.jsp";
+		}
+
+		@RequestMapping("/joinForm.trip")
+		public String join(){
+				
+				return "/trip.member/joinForm.jsp";
+					
+		}
+		
+		@RequestMapping("/joinPro.trip")
+		public String joinPro(LoginDTO dto){
+			
+			sqlmap.insert("joinInsert", dto);
+			return "/trip.member/joinPro.jsp";
+			
+		}
+		@RequestMapping("/deleteForm.trip")
+		public String deleteForm(){
+			return "/trip.member/deleteForm.jsp";
+			
+		}
+		@RequestMapping("/deletePro.trip")
+		public String deletePro(HttpSession session,LoginDTO dto, HttpServletRequest request){
+			
+			String dbid = (String) session.getAttribute("memId");
+			String dbpw = (String) sqlmap.queryForObject("pw", dbid);
+			String pw = request.getParameter("pw");
+			
+			if(dbpw.equals(pw)){			
+			                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+				dto=(LoginDTO)sqlmap.queryForObject("delete",dbid);
+				
+			}
+			return "/trip.member/deletePro.jsp";
+			
+		}
+
+			
+	}
+
---
+++ src/trip/member/LoginDTO.java
@@ -0,0 +1,47 @@
+package trip.member;
+
+
+	public class LoginDTO {
+		private String id;
+		private String pw;
+		private String name;
+		private String domain;
+		private String path;
+		
+		public String getPath() {
+			return path;
+		}
+		public void setPath(String path) {
+			this.path = path;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getDomain() {
+			return domain;
+		}
+		public void setDomain(String domain) {
+			this.domain = domain;
+		}
+		public String getId() {
+			return id;
+		}
+		public void setId(String id) {
+			this.id = id;
+		}
+		public String getPw() {
+			return pw;
+		}
+		public void setPw(String pw) {
+			this.pw = pw;
+		}
+		
+		
+
+	}
+
+
+
